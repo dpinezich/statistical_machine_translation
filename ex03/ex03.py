135bb4767eabdf854d6af1600e505cb2ae9ee0a3
dpinezich <david.pinezich@gmail.com>
2017-04-01 16:55:13 +0200

    skeleton ready

---
+++ ex03/ex03.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# __author__ David Pinezich <s11729464>
+# __author__ Christian Schneider <s10606002>
+
+# main programm
+def main():
+    print
+    print "------------------------------"
+    print u"Implement IBM Model 1"
+    print "------------------------------"
+    print
+
+    while True:
+        try:
+            input = raw_input("Okay do you really want to start this? q=quit, anything else starts the program")
+        except:
+            print "Bad input, closing"
+            exit(0)
+        # quit
+        if input == 'q':
+            print u"Sad to see you going.... Bye!"
+            exit(0)
+        else:
+            break;
+
+if __name__ == '__main__':
+    main()

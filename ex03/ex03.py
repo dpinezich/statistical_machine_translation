07de4abdde24159f72d23fa46f978145d64ecf18
dpinezich <david.pinezich@gmail.com>
2017-04-02 23:35:15 +0200

    better approach of lining information

--- ex03/ex03.py
+++ ex03/ex03.py
@@ -4,7 +4,6 @@
 # __author__ David Pinezich <s11729464>
 # __author__ Christian Schneider <s10606002>
 
-import re
 
 # main programm
 def main():
@@ -27,9 +26,12 @@ def main():
         else:
             break;
 
+    number_of_words = 2
 
     save_line = []
     text_to_line = {}
+
+    # open the corpus and store all phrases with the length expected
     with open("align/corpus.es-en", "r") as corpus:
         for index, c in enumerate(corpus):
             splitted_line = c.split("|||")
@@ -37,18 +39,24 @@ def main():
             count_es = len(splitted_line[0].split())
             count_en = len(splitted_line[1].split())
 
-            if (count_es < 3 and count_en < 3):
-                text_to_line[index] = str(splitted_line[0]) + "|||" + str(splitted_line[1])
+            if (count_es <= number_of_words and count_en <= number_of_words):
+                text_to_line[index] = ["index", str(splitted_line[0]).strip().split(), str(splitted_line[1]).strip().split()]
+                # save all relevant lines for a faster gdfa searching
                 save_line.append(index)
-    corpus.close()
 
+    corpus.close()
+    print u"corpus is ready"
 
     with open("align/alignments.gdfa", "r") as alignment:
         for index, a in enumerate(alignment):
             if (index in save_line):
-                print str(index) + ": " + str(a) + text_to_line[index]
+                text_to_line[index][0] = a.strip()
 
     alignment.close()
+    print u"alignment is ready"
+
+    for i in text_to_line:
+        print i
 
 if __name__ == '__main__':
     main()
